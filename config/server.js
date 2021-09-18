983f5bc0f0c1aca34172361724d0f6838395c3e3
fajri1 <achmad17nov@gmail.com>
2021-09-18 12:16:41 +0700

    update: models

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ad40e80171ba99b75f485aa089933bfc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1510d74f-1ca6-485d-bb54-61f62f4b8341'
+};
