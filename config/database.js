fdc3c90cd46b42ea68c463179cc147185d241990
fajri1 <achmad17nov@gmail.com>
2021-09-18 19:29:07 +0700

    update: database config for production

--- config/database.js
+++ config/database.js
@@ -1,49 +1,20 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-  if(env('NODE_ENV') === 'production'){
-    const config = parse(process.env.DATABASE_URL);
-
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-          },
-          options: {
-            ssl: false
-          },
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql2',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'coursedb'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', ''),
       },
-    }
-  }
-  
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'mysql2',
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 3306),
-          database: env('DATABASE_NAME', 'coursedb'),
-          username: env('DATABASE_USERNAME', 'root'),
-          password: env('DATABASE_PASSWORD', ''),
-        },
-        options: {
-          ssl: false
-        },
+      options: {
+        ssl: false
       },
     },
-  }
-  
-};
+  },
+});
  
